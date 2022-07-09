8917c7f3873e35b8989e6c873b8c01e6f023f236
Roby Awaluddin <61074887+roby-aw@users.noreply.github.com>
2022-07-09 16:04:24 +0700

    add: modules app

---
+++ app/modules/modules.go
@@ -0,0 +1,12 @@
+package modules
+
+import (
+	"api-redeem-point/api"
+	"api-redeem-point/config"
+	"api-redeem-point/utils"
+)
+
+func RegistrationModules(dbCon *utils.DatabaseConnection, config *config.AppConfig) api.Controller {
+	controller := api.Controller{}
+	return controller
+}

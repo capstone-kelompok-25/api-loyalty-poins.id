f0b3061941191c3c0037b5ac3ac9e44b776b019d
Roby Awaluddin <61074887+roby-aw@users.noreply.github.com>
2022-07-10 19:20:19 +0700

    update modules app for feature admin

--- app/modules/modules.go
+++ app/modules/modules.go
@@ -2,9 +2,12 @@ package modules
 
 import (
 	"api-redeem-point/api"
+	adminApi "api-redeem-point/api/admin"
 	customerApi "api-redeem-point/api/customer"
+	adminBusiness "api-redeem-point/business/admin"
 	customerBusiness "api-redeem-point/business/customer"
 	"api-redeem-point/config"
+	adminRepo "api-redeem-point/repository/admin"
 	customerRepo "api-redeem-point/repository/customer"
 	"api-redeem-point/utils"
 )
@@ -14,8 +17,13 @@ func RegistrationModules(dbCon *utils.DatabaseConnection, config *config.AppConf
 	customerPermitService := customerBusiness.NewService(customerPermitRepository)
 	customerPermitController := customerApi.NewController(customerPermitService)
 
+	adminPermitRepository := adminRepo.RepositoryFactory(dbCon)
+	adminPermitService := adminBusiness.NewService(adminPermitRepository)
+	adminPermitController := adminApi.NewController(adminPermitService)
+
 	controller := api.Controller{
 		CustomerController: customerPermitController,
+		AdminController:    adminPermitController,
 	}
 	return controller
 }

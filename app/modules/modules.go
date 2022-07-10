05c0ddbd323c04caebbf5778e12468f93e0e00a5
Roby Awaluddin <61074887+roby-aw@users.noreply.github.com>
2022-07-10 19:11:43 +0700

    update modules app for feature store

--- app/modules/modules.go
+++ app/modules/modules.go
@@ -3,9 +3,12 @@ package modules
 import (
 	"api-redeem-point/api"
 	customerApi "api-redeem-point/api/customer"
+	storeApi "api-redeem-point/api/store"
 	customerBusiness "api-redeem-point/business/customer"
+	storeBusiness "api-redeem-point/business/store"
 	"api-redeem-point/config"
 	customerRepo "api-redeem-point/repository/customer"
+	storeRepo "api-redeem-point/repository/store"
 	"api-redeem-point/utils"
 )
 
@@ -14,8 +17,12 @@ func RegistrationModules(dbCon *utils.DatabaseConnection, config *config.AppConf
 	customerPermitService := customerBusiness.NewService(customerPermitRepository)
 	customerPermitController := customerApi.NewController(customerPermitService)
 
+	storePermitRepository := storeRepo.RepositoryFactory(dbCon)
+	storePermitService := storeBusiness.NewService(storePermitRepository)
+	storePermitController := storeApi.NewController(storePermitService)
 	controller := api.Controller{
 		CustomerController: customerPermitController,
+		StoreController:    storePermitController,
 	}
 	return controller
 }

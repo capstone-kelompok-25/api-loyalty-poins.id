e56b86255c6b47588bef4a94a85b47ce888c009a
Roby Awaluddin <61074887+roby-aw@users.noreply.github.com>
2022-07-10 17:27:43 +0700

    add modules api customer

--- app/modules/modules.go
+++ app/modules/modules.go
@@ -2,11 +2,20 @@ package modules
 
 import (
 	"api-redeem-point/api"
+	customerApi "api-redeem-point/api/customer"
+	customerBusiness "api-redeem-point/business/customer"
 	"api-redeem-point/config"
+	customerRepo "api-redeem-point/repository/customer"
 	"api-redeem-point/utils"
 )
 
 func RegistrationModules(dbCon *utils.DatabaseConnection, config *config.AppConfig) api.Controller {
-	controller := api.Controller{}
+	customerPermitRepository := customerRepo.RepositoryFactory(dbCon)
+	customerPermitService := customerBusiness.NewService(customerPermitRepository)
+	customerPermitController := customerApi.NewController(customerPermitService)
+
+	controller := api.Controller{
+		CustomerController: customerPermitController,
+	}
 	return controller
 }
